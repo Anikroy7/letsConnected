d105bad45a893c3267d759a6aa03a9ac762a0f53
Anikroy7 <roy645800@gmail.com>
2022-11-21 16:37:57 +0600

    fix the bug

--- php/users.php
+++ php/users.php
@@ -6,7 +6,7 @@ $outgoing_id = $_SESSION['unique_id'];
 $sql = mysqli_query($conn, "SELECT *  FROM users WHERE NOT unique_id= {$outgoing_id}");
 $output = '';
 if (mysqli_num_rows($sql) == 1) {
-    $output = "<p class='text-center text-gray-500'>No users found for chat with you</p>";
+    $output .= "<p class='text-center text-gray-500'>No users found for chat with you</p>";
 } elseif (mysqli_num_rows($sql) > 0) {
     include 'usersData.php';
 }
--- php/usersData.php
+++ php/usersData.php
@@ -1,19 +1,21 @@
 <?php
 
+
 while ($row = mysqli_fetch_assoc($sql)) {
-    include_once 'config.php';
-    $sql2 = mysqli_query($conn, "SELECT * FROM messages WHERE (incoming_msg_id= {$row['unique_id']}) OR (outgoing_msg_id= {$row['unique_id']}) AND (outgoing_msg_id= {$outgoing_id}) OR (incoming_msg_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1");
-
-    $row2 = mysqli_fetch_assoc($sql2);
-
-    if (mysqli_num_rows($sql2) > 0) {
-        $result = $row2['message'];
+    $sql2 = "SELECT * FROM messages WHERE (incoming_msg_id = {$row['unique_id']}
+                OR outgoing_msg_id = {$row['unique_id']}) AND (outgoing_msg_id = {$outgoing_id} 
+                OR incoming_msg_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1";
+    $query2 = mysqli_query($conn, $sql2);
+    $row2 = mysqli_fetch_assoc($query2);
+    (mysqli_num_rows($query2) > 0) ? $result = $row2['message'] : $result = "No message available";
+    (strlen($result) > 28) ? $msg =  substr($result, 0, 28) . '...' : $msg = $result;
+    if (isset($row2['outgoing_msg_id'])) {
+        ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: " : $you = "";
     } else {
-        $result = "No message available";
+        $you = "";
     }
+    ($row['status'] == "Offline now") ? $offline = "offline" : $offline = "";
 
-    (strlen($result) > 25) ? $msg = substr($result, 0, 28) . '...' : $msg = $result;
-    ($outgoing_id == $row2['outgoing_msg_id'] ? $you = "You:" : $you = '');
 
 
     $output .= '<a href="chat.php?user_id=' . $row['unique_id'] . '">
